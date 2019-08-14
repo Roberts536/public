2b8e00d6a8acac5d4aa58ebcc5be28bbee5a6c79
James Roberts <j.roberts536@gmail.com>
2019-08-14 19:41:13 +0100

    start aes project
    Start building an amateur implementation of 128b AES in ECB mode.
    Project does not yet compile.

---
+++ aes/aes/aes.cpp
@@ -0,0 +1,15 @@
+/*
+An amateur implementation of AES in 128-bit ECB mode.
+For experimentation only; do not use where real security is required.
+*/
+
+#include <iostream>
+#include <bitset>
+
+int main()
+{
+	// The 128b key is initialised as an array of four 32b words
+	const std::bitset<32> KEY[4];
+
+    std::cout << "Hello World!\n"; 
+}
