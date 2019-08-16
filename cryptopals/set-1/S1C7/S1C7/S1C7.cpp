b0864a6d68f0d7b79043303f516edd70148c5143
James Roberts <j.roberts536@gmail.com>
2019-08-16 18:37:13 +0100

    complete set 1 challenge 7

--- cryptopals/set-1/S1C7/S1C7/S1C7.cpp
+++ cryptopals/set-1/S1C7/S1C7/S1C7.cpp
@@ -1,70 +1,100 @@
+#include "osrng.h"
+using CryptoPP::AutoSeededRandomPool;
+using CryptoPP::byte;
+
 #include <iostream>
-#include <string>
+using std::cout;
+using std::cerr;
+using std::endl;
+
 #include <fstream>
-#include <algorithm>
-#include <openssl/evp.h>
-#include <openssl/conf.h>
-#include <openssl/err.h>
+#include <sstream>
+
+#include <string>
+using std::string;
+
+#include <cstdlib>
+using std::exit;
+
+#include "cryptlib.h"
+using CryptoPP::Exception;
+
+#include "hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+
+#include "filters.h"
+using CryptoPP::StringSink;
+using CryptoPP::StringSource;
+using CryptoPP::StreamTransformationFilter;
+
+#include "aes.h"
+using CryptoPP::AES;
+
+#include "modes.h"
+using CryptoPP::ECB_Mode;
 
 #include "base64.h"
+std::string base64_decode(std::string const& encoded_string);
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-	unsigned char *iv, unsigned char *plaintext);
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-	unsigned char *iv, unsigned char *ciphertext);
-
-int main()
+int main(int argc, char* argv[])
 {
-	// Initialise crypto libraries
-	OpenSSL_add_all_ciphers();
-	OpenSSL_add_all_digests();
+	AutoSeededRandomPool prng;
 
-	// Get the first line from the file
-	std::string fromFile{ "CRIwqt4+szDbqkNY+I0qbDe3LQz0wiw0SuxBQtAM5TDdMbjCMD/venUDW9BL" };
-	fromFile = base64_decode(fromFile);
+	// Use the key given by Cryptopals
+	const char key_text[]{ "YELLOW SUBMARINE" };
+	byte key[AES::DEFAULT_KEYLENGTH];
+	for (int i = 0; i < sizeof(key); i++)
+	{
+		key[i] = key_text[i];
+	}
 
-	// A 128 bit key
-	unsigned char *key = (unsigned char *)"YELLOW SUBMARINE";
+	string cipher, recovered;
 
-	// A 128 bit IV
-	unsigned char *iv = (unsigned char *)"0123456789012345";
+	// Open the ciphertext input filestream
+	std::string in_filename{ "7.txt" };
+	std::ifstream inf(in_filename);
+	// If there's a problem opening the ifstream
+	if (!inf)
+	{
+		// Tell the user the problem, and exit
+		std::cerr << "Could not open file: "
+			<< in_filename << " for reading.\n";
+		return 1;
+	}
 
-	// Message to be encrypted
-	unsigned char *plaintext =
-		(unsigned char *)"I'm back and I'm having fun with all of my friends, who are also having fun themselves.";
+	// Keep pulling input from the file while we can
+	while (inf)
+	{
+		// Pull a line off the input filestream
+		string line;
+		getline(inf, line);
 
-	/*
-	 * Buffer for ciphertext. Ensure the buffer is long enough for the
-	 * ciphertext which may be longer than the plaintext, depending on the
-	 * algorithm and mode.
-	 */
-	unsigned char ciphertext[128];
+		// Decrypt (uses 3rd party stuff) and add to sstream
+		cipher += base64_decode(line);
+	}
 
-	/* Buffer for the decrypted text */
-	unsigned char decryptedtext[128];
+	// Decrypt
+	try
+	{
+		// Initialise the Decryption object and feed it the key
+		ECB_Mode< AES >::Decryption d;
+		d.SetKey(key, sizeof(key));
 
-	int decryptedtext_len, ciphertext_len;
+		// Strip the padding as required, and decode
+		StringSource s(cipher, true,
+			new StreamTransformationFilter(d,
+				new StringSink(recovered)
+			)
+		);
 
-	/* Encrypt the plaintext */
-	ciphertext_len = encrypt(plaintext, strlen((char *)plaintext), key, iv,
-		ciphertext);
-
-	/* Do something useful with the ciphertext here */
-	printf("Ciphertext is:\n");
-	BIO_dump_fp(stdout, (const char *)ciphertext, ciphertext_len);
-
-	/* Decrypt the ciphertext */
-	decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
-		decryptedtext);
-
-	/* Add a NULL terminator. We are expecting printable text */
-	decryptedtext[decryptedtext_len] = '\0';
-
-	/* Show the decrypted text */
-	printf("Decrypted text is:\n");
-	printf("%s\n", decryptedtext);
-
+		cout << "Recovered text: " << recovered << endl;
+	}
+	catch (const CryptoPP::Exception& e)
+	{
+		cerr << e.what() << endl;
+		std::exit(1);
+	}
 
 	return 0;
-
 }
