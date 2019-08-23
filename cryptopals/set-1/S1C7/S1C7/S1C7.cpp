a666f8feddacd6afe3fce28d0236e35dbce482b6
James Roberts <j.roberts536@gmail.com>
2019-08-23 10:41:30 +0100

    update S1C7 to use header file

--- cryptopals/set-1/S1C7/S1C7/S1C7.cpp
+++ cryptopals/set-1/S1C7/S1C7/S1C7.cpp
@@ -1,42 +1,9 @@
-#include "osrng.h"
-using CryptoPP::AutoSeededRandomPool;
-using CryptoPP::byte;
+#include "S1C7.h"
 
-#include <iostream>
-using std::cout;
-using std::cerr;
-using std::endl;
-
-#include <fstream>
-#include <sstream>
-
-#include <string>
-using std::string;
-
-#include <cstdlib>
-using std::exit;
-
-#include "cryptlib.h"
-using CryptoPP::Exception;
-
-#include "hex.h"
-using CryptoPP::HexEncoder;
-using CryptoPP::HexDecoder;
-
-#include "filters.h"
-using CryptoPP::StringSink;
-using CryptoPP::StringSource;
-using CryptoPP::StreamTransformationFilter;
-
-#include "aes.h"
-using CryptoPP::AES;
-
-#include "modes.h"
-using CryptoPP::ECB_Mode;
-
-#include "base64.h"
-std::string base64_decode(std::string const& encoded_string);
-
+/*
+Set 1, Challenge 7 of the Cryptopals Challenges.
+Decrypt a file with AES in ECB mode and a known key.
+*/
 int main(int argc, char* argv[])
 {
 	AutoSeededRandomPool prng;
