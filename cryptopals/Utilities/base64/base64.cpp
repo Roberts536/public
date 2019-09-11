58525e2b3b213e255e7503de6e441b25b0400837
James Roberts <j.roberts536@gmail.com>
2019-09-11 15:50:39 +0100

    Begin work on a base64 decoding function

--- cryptopals/Utilities/base64/base64.cpp
+++ cryptopals/Utilities/base64/base64.cpp
@@ -69,3 +69,41 @@ std::string base64_encode(const std::string input)
 	
 	return result;
 }
+
+/*
+// Base64 decode an input string. Beware this may produce unprintables.
+std::string base64_decode(std::string input)
+{
+	std::string result;
+
+	// Sets of four base64 characters ('chunks') decode to three chars
+	int numChunks = (int)ceilf(input.length() / (float)4);
+	for (int i = 0; i < numChunks; i++)
+	{
+		std::string chunk{ input.substr(4 * i, 4) };
+		std::bitset<24> chunkBits(0);
+		for (byte letter : chunk)
+		{
+			// Only the least significant 6 bits contain data
+			chunkBits <<= 6;
+			std::bitset<24> letterBits(letter);
+			chunkBits |= letterBits;
+		}
+
+		// Some base64 implementations pad the end with '='
+		// TODO
+		std::bitset<24> twoEquals(15485); // 0..0 0..0 00111101 00111101
+
+		for (int j = 0; j < 3; j++)
+		{
+			std::bitset<8> rawByteBits;
+			for (int k = 0; k < 8; k++)
+			{
+				rawByteBits[k] = chunkBits[8 * (2 - j) + k];
+			}
+			result += (byte)rawByteBits.to_ulong();
+		}
+	}
+	return result;
+}
+*/
