b9e2a3eae4846278940972428efa98e521b83594
James Roberts <j.roberts536@gmail.com>
2019-10-19 19:59:47 +0100

    add files for abstract factory demo

---
+++ study/Patterns/abstract-factory/AbstractFactoryPatternDemo.java
@@ -0,0 +1,24 @@
+/*
+ * A class demonstrating the Abstract Factory Pattern.
+ *
+ * The Abstract Factory Pattern revolves around creating a 'factory of factories'.
+ *
+ * The code in this demo follows the walkthrough on TutorialsPoint; it is not my own work.
+ *
+ */
+public class AbstractFactoryPatternDemo {
+   public static void main(String[] args) {
+
+      AbstractFactory shapeFactory = FactoryProducer.getFactory(false);
+      Shape shape1 = shapeFactory.getShape("RECTANGLE");
+      shape1.draw();
+      Shape shape2 = shapeFactory.getShape("SQUARE");
+      shape2.draw();
+
+      AbstractFactory shapeFactory1 = FactoryProducer.getFactory(true);
+      Shape shape3 = shapeFactory1.getShape("RECTANGLE");
+      shape3.draw();
+      Shape shape4 = shapeFactory1.getShape("SQUARE");
+      shape4.draw();
+   }
+}
---
+++ study/Patterns/abstract-factory/RoundedRectangle.java
@@ -0,0 +1,6 @@
+public class RoundedRectangle implements Shape {
+	@Override
+	public void draw() {
+		System.out.println("Inside the RoundedRectangle::draw() method.");
+	}
+}
